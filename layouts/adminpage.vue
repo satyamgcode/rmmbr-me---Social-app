caf0fea950ef64b07529a79f90215341cd951105
satyamgcode <guptasatyamcode@gmail.com>
2024-11-15 13:56:06 +0530

    initial commit

---
+++ layouts/adminpage.vue
@@ -0,0 +1,12 @@
+<script setup>
+// 
+</script>
+
+<template>
+<div class="bg-black text-white">
+<p>
+    here is the link
+</p>
+<slot></slot>
+</div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,9 @@
+<script setup>
+definePageMeta({
+  layout: 'adminpage',
+});
+</script>
+
+<template>
+<h1 class="text-3xl"> added new NuxtPage</h1>
+</template>
